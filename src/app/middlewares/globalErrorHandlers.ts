bd1c6504cb31d3c5d932ffdf88522336b4ac7d42
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2025-11-28 17:35:34 +0600

    duplicate error, cast error handling added

--- src/app/middlewares/globalErrorHandlers.ts
+++ src/app/middlewares/globalErrorHandlers.ts
@@ -3,14 +3,47 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import AppError from "../ErrorHelpers/appError";
+import { Profile } from 'passport-google-oauth20';
 
 export const globalErrorHandlers=((err:any, req:Request, res:Response,next:NextFunction) => {
 
    let  statusCode = 500;
     let message = `Something went wrong! ${err.message}`;
 
-  
-    if (err instanceof AppError){
+    const errorSources:any = [
+      //   {
+      //   path:"isDeleted",
+      //   message: "Cast Failed"
+      //  }
+      ];
+
+    if(err.code === 11000){
+      console.log("Duplicate Error")
+        statusCode = 400;
+       const duplicate = err.message.match(/"([^"]*)"/);
+        message = `Duplicate value entered for ${duplicate ? duplicate[1] : 'field'}. Please choose another value!`;
+
+    }else if (err.name === "CastError"){
+
+        statusCode = 400;
+         message = `Invalid MongoDB ObjectID. Profile not found with id: ${err.value}`;
+       
+    } else if(err.name === "ValidationError"){
+        statusCode = 400;
+      const errors = Object.values(err.errors);
+
+      errors.forEach((el: any) => {
+        // errorSources.push(el.path);
+
+        errorSources.push({
+          path: el.path,
+          message: el.message,
+        });
+      });
+      // console.log(errorSources); 
+      message = err.message;
+    }
+    else if (err instanceof AppError){
         statusCode = err.statusCode;
         message = err.message;
     }else if(err instanceof Error){
@@ -21,7 +54,8 @@ export const globalErrorHandlers=((err:any, req:Request, res:Response,next:NextF
   res.status(statusCode).json({
     success: false,
     message,
-    err,
+    errorSources,
+    // err,
     stack: envVars.NODE_ENV === "development" ? err.stack : null,
   })
 });
