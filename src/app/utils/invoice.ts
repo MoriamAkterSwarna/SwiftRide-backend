0239e895f866080f104ee1ecd92d3abf5b8136cb
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2026-01-01 17:31:57 +0600

    Invoice with pdfkit is ready

---
+++ src/app/utils/invoice.ts
@@ -0,0 +1,65 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import PDFDocument from "pdfkit"; 
+import AppError from "../ErrorHelpers/appError";
+
+export interface IInvoiceData {
+   transactionId: string;
+   bookingDate: Date;
+   customerName: string;
+   rideTitle: string;
+   maxGuests: number;
+   totalAmount: number;
+   
+}
+
+export const generatePdf = async (invoiceData: IInvoiceData) => {
+    
+
+    try {
+
+        return new Promise ((resolve, reject) => {
+
+            const doc = new PDFDocument({ size: 'A4', margin: 50 }); 
+
+            const buffer:Uint8Array[] = [];
+
+            doc.on("data", (chunk) => buffer.push(chunk));
+            doc.on("end", () => resolve(Buffer.concat(buffer)));
+            doc.on("error", (error) => reject(error));
+
+
+            // PDF content 
+
+            doc.fontSize(25).text("Invoice", { align: "center" });
+            doc.moveDown();
+
+            doc.fontSize(15).text(`Transaction ID: ${invoiceData.transactionId}`);
+            doc.moveDown();
+
+            doc.fontSize(15).text(`Booking Date : ${invoiceData.bookingDate}`);
+            doc.moveDown();
+
+            doc.fontSize(15).text(`Customer Name : ${invoiceData.customerName}`);
+            doc.moveDown();
+
+            doc.fontSize(15).text(`Ride Title : ${invoiceData.rideTitle}`);
+            doc.moveDown();
+
+            doc.fontSize(15).text(`Max Guests : ${invoiceData.maxGuests}`);
+            doc.moveDown();
+
+            doc.fontSize(15).text(`Total Amount : ${invoiceData.totalAmount}`);
+            doc.moveDown();
+
+            doc.text("Thank you for using our service!", { align: "center" });
+            doc.end();
+            
+        })
+        
+    } catch (error:any) {
+        
+        console.log(error)
+        throw new AppError(500, `Failed to generate PDF ${error?.message}`)
+        
+    }
+}
