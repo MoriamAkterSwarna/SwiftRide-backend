33ab9e1a0104a2ff9dcfae42ea4a4d4cc378dde1
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2026-01-01 23:06:51 +0600

    fix: resolve package dependency conflicts and update deployment config

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,15 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
-import { ZodObject } from "zod";
+
 
 export const validateRequest =
-  (zodSchema: ZodObject) =>
+  (zodSchema: any) =>
   async (req: Request, res: Response, next: NextFunction) => {
     try {
+      if (req.body.data) {
+        req.body = JSON.parse(req.body.data);
+      }
 
-    if(req.body.data){
-      req.body = JSON.parse(req.body.data);
-    }
-      
       req.body = await zodSchema.parseAsync(req.body);
       next();
     } catch (error) {
